0257652a03e5aeb0e0704be79ccdb0ab6e5933f0
YUFFRY <mathiasfernandezvalenzuela@gmail.com>
2025-10-18 16:38:40 -0500

    Primer commit con mi proyecto

---
+++ ucvBackend/src/main/java/com/ucv/UcvBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ucv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcvBackendApplication {
+
+	// Metodo que inicia en el backend
+	public static void main(String[] args) {
+		SpringApplication.run(UcvBackendApplication.class, args);
+	}
+
+}
