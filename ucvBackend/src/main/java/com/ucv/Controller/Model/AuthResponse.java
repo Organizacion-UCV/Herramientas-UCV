b83523f5258c0cc44dff688052febe49e5a486dd
YUFFRY <mathiasfernandezvalenzuela@gmail.com>
2025-11-13 11:35:36 -0500

    Cambios Usuario

--- ucvBackend/src/main/java/com/ucv/Controller/Model/AuthResponse.java
+++ ucvBackend/src/main/java/com/ucv/Controller/Model/AuthResponse.java
@@ -1,5 +1,17 @@
 package com.ucv.Controller.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
-    
+
+    // Atributos
+    private String token;
+
 }
--- ucvBackend/src/main/java/com/ucv/Controller/Model/RegisterRequest.java
+++ ucvBackend/src/main/java/com/ucv/Controller/Model/RegisterRequest.java
@@ -1,5 +1,22 @@
 package com.ucv.Controller.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
-    
+
+    // Atributos
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String nickname;
+    private String password;
+    private String cargo;
 }
