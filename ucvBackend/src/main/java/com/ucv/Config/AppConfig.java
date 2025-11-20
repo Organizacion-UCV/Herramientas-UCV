52b7d4edc1180e1d8ba636f0100dc8f1115b99ec
YUFFRY <mathiasfernandezvalenzuela@gmail.com>
2025-11-20 09:30:25 -0500

    DOC

--- ucvBackend/src/main/java/com/ucv/Config/AppConfig.java
+++ ucvBackend/src/main/java/com/ucv/Config/AppConfig.java
@@ -57,7 +57,7 @@ public class AppConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://herramientas-ucv-22.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
